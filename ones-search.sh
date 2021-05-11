fd27eca1658796bcdabf2258514dfb61d8e10e8d
moli <molix2@outlook.com>
2021-05-12 00:00:15 +0800

    init

---
+++ ones-search.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# Required parameters:
+# @raycast.schemaVersion 1
+# @raycast.title ONES Search
+# @raycast.mode compact
+
+# Optional parameters:
+# @raycast.icon 🤖
+# @raycast.argument1 { "type": "text", "placeholder": "Placeholder" }
+# @raycast.packageName ones
+
+# Documentation:
+# @raycast.author moli
+# @raycast.authorURL https://github.com/molizz
+
+echo "Hello World! Argument1 value:  "$1")"
+
